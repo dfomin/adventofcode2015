6fe2b81fb51718266746b4266dc1d1f154ecb314
Dmitry Fomin <fomin.dm@gmail.com>
2023-04-29 13:06:01 +0200

    Day 1

---
+++ day1_1/src/main.rs
@@ -0,0 +1,15 @@
+use std::fs;
+
+fn main() {
+    let contents = fs::read_to_string("../inputs/day1_1.txt").expect("Input file not found");
+    let mut result = 0;
+    for ch in contents.trim().chars() {
+        match ch {
+            '(' => result += 1,
+            ')' => result -= 1,
+            _ => panic!("Unexpected char"),
+        }
+    }
+
+    println!("{}", result);
+}
---
+++ day1_2/src/main.rs
@@ -0,0 +1,21 @@
+use std::fs;
+
+fn main() {
+    let contents = fs::read_to_string("../inputs/day1_1.txt").expect("Input file not found");
+    let mut result = 0;
+    let mut position: Option<i32> = None;
+    for (i, ch) in contents.trim().chars().enumerate() {
+        match ch {
+            '(' => result += 1,
+            ')' => result -= 1,
+            _ => panic!("Unexpected char"),
+        }
+
+        if result == -1 {
+            position = Some(i as i32 + 1);
+            break;
+        }
+    }
+
+    println!("{:?}", position);
+}
